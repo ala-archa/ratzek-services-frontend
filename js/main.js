784cb06334bc814bceda1f85be58309f2ebb58e9
xkochevnikx <xkochevnik@protonmail.com>
2023-02-19 22:04:13 +0600

    кнопочный вредитель не трогай мои кнопки!

--- js/main.js
+++ js/main.js
@@ -1,29 +1,49 @@
-const btn = document.querySelector("button");
-const testBnt = document.querySelector("#testBtn");
-btn.addEventListener("click", clickTest);
-
+const btn = document.querySelector('button');
+const testBnt = document.querySelector('#testBtn');
 let sec = 10;
-function tickButton() {
-  sec -= 1;
-  btn.innerText = `Подождите ${sec} сек`;
-  if (sec === 0) {
-    btn.innerText = "МОЖНО ВОЙТИ";
-    btn.style.backgroundColor = "rgb(106, 194, 72)";
-    btn.removeAttribute("disabled");
-    clearInterval(interval);
-  }
+btn.addEventListener('click', entrance);
+// const API = '/api/v1/client';
+const API = 'http://82.146.59.228:5001/api/v1/client';
+
+ping(interval);
+
+function ping(interval) {
+   fetch(API)
+      .then((resp) => resp.json())
+      .then((resp) => {
+         if (resp.internet_connection_status === 'Inactive') {
+            btn.style.display = 'block';
+            interval();
+         } else if (resp.internet_connection_status === 'Connected') {
+            btn.innerText = ``;
+         }
+      })
+      .catch((error) => {
+         console.error(error);
+      });
 }
 
-tickButton();
+function interval() {
+   const int = setInterval(() => {
+      sec -= 1;
+      btn.innerText = `до входа осталось ${sec}`;
+      if (sec === 0) {
+         btn.innerText = 'МОЖНО ВОЙТИ';
+         btn.style.backgroundColor = 'rgb(106, 194, 72)';
+         btn.removeAttribute('disabled');
+         clearInterval(int);
+      }
+   }, 1000);
+}
 
-const interval = setInterval(tickButton, 1000);
-
-function clickTest() {
-  fetch("/api/v1/client", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json; charset=utf-8",
-    },
-    body: '',
-  });
+function entrance() {
+   fetch(API, {
+      method: 'POST',
+      headers: {
+         'content-type': 'application/json; charset=utf-8',
+      },
+      body: '',
+   }).catch((error) => {
+      console.error(error);
+   });
 }
