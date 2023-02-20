c1e955c23f1a79f648d6c8b929276b950d812f4c
xkochevnikx <xkochevnik@protonmail.com>
2023-02-20 21:29:00 +0600

    три условия

--- js/main.js
+++ js/main.js
@@ -8,17 +8,23 @@ const API = 'http://82.146.59.228:5001/api/v1/client';
 ping(interval);
 
 function ping(interval) {
-   fetch('http://localhost:8000/connect')
+   fetch('http://82.146.59.228:5001/api/v1/client')
       .then((resp) => resp.json())
       .then((resp) => {
          console.log(resp);
-         if (resp.internet_connection_status === 'Inactive') {
+         if (resp.is_internet_available) {
             btn.style.display = 'block';
             interval();
-         } else if (resp.internet_connection_status === 'Connected') {
+         } else if (
+            resp.is_internet_available ||
+            resp.internet_connection_status.Connected.bytes_sent > 0
+         ) {
             btn.style.display = 'block';
-            btn.innerText = `Вы израсходовали ${resp.bytes_sent} из ${resp.bytes_unlimited_limit} на безлимитной скорости.`;
+            btn.innerText = `Вы израсходовали ${resp.internet_connection_status.Connected.bytes_sent} из ${resp.internet_connection_status.Connected.bytes_unlimited_limit} на безлимитной скорости.`;
             btn.style.backgroundColor = 'rgb(106, 194, 72)';
+         } else if (!resp.is_internet_available) {
+            btn.style.display = 'block';
+            btn.innerText = `К сожалению доступа в данный момент нет`;
          }
       })
       .catch((error) => {
@@ -40,7 +46,13 @@ function interval() {
 }
 
 function entrance() {
-   fetch('http://localhost:8000/connect')
+   fetch('http://82.146.59.228:5001/api/v1/client', {
+      method: 'POST',
+      headers: {
+         'content-type': 'application/json; charset=utf-8',
+      },
+      body: '',
+   })
       .then(() => console.log('ыы'))
       .catch((error) => {
          console.error(error);
